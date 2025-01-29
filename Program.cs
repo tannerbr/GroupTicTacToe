950eb45e8e80af52107811ecd497d2d138f81a0a
tannerbr <tannerjbriggs@hotmail.com>
2025-01-29 08:35:43 -0700

    Small changes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Welcome the User 
+Console.WriteLine("Welcome to Tic-Tac-Toe");
+
+// Create a game board array to store the players’ choices
+string[] gameArray = [];
+
+// Ask each player in turn for their choice and update the game board array
+Console.WriteLine("Please enter which space you want to move to");
+Console.ReadLine();
+// Print the board by calling the method in the supporting class
+// PrintBoard();
+
+// Check for a winner by calling the method in the supporting class, and notify the players
+// When a win has occurred and which player won the game
+// CheckWinner();
+
+
